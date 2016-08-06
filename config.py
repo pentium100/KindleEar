83f6b2329fcb2863d712d08bcf470834663c96a6
pentium100 <liwei77@hotmail.com>
2016-08-06 14:44:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ KindleEar配置文件，请务必修改开始两个配置（如果使用uploader
 """
 
 SRC_EMAIL = "liwei99@gmail.com"  #Your gmail account for sending mail to Kindle
-DOMAIN = "https://liwei100.appspot.com" #Your domain of app
+DOMAIN = "https://liweixm2.appspot.com" #Your domain of app
 
 TIMEZONE = 8  #Default timezone, you can modify it in webpage after deployed
 
